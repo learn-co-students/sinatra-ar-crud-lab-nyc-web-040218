1d7e176bfbf2e234fc5f700f603c335c658a8be2
Ahamed <ahamedali95@gmail.com>
2018-04-25 16:11:34 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,55 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
+  get '/posts/new' do
+    erb :new
   end
+
+  #CREATE
+  post "/posts" do
+    Post.create(name: params[:name], content: params[:content])
+    @posts = Post.all
+
+    erb :index
+  end
+
+  #READ
+  get "/posts" do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  #READ
+  get "/posts/:id" do
+    @post = Post.find_by(id: params[:id].to_i)
+
+    erb :show
+  end
+
+  #UPDATE
+  get "/posts/:id/edit" do
+    @post = Post.find_by(id: params[:id].to_i)
+
+    erb :edit
+  end
+
+  #UPDATE
+  patch "/posts/:id" do
+    @post = Post.find_by(id: params[:id].to_i)
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+
+    erb :show
+  end
+
+  #DELETE
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id].to_i)
+    @post.delete
+
+    erb :delete
+  end
+
 end
